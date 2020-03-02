88c261263ab953758d97770f710acfcf74bb8133
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-03-02 17:38:43 +0800

    Entity Model vs 面向外部的Model

--- Routine.Api/Controllers/CompaniesController.cs
+++ Routine.Api/Controllers/CompaniesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Models;
 using Routine.Api.Services;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,7 +27,17 @@ namespace Routine.Api.Controllers
         {
             var companies = await _companyRepository.GetCompaniesAsync();
 
-            return Ok(companies);
+            var companyDtos = new List<CompanyDto>();
+            foreach (var company in companies)
+            {
+                companyDtos.Add(new CompanyDto
+                {
+                    Id = company.Id,
+                    Name = company.Name
+                });
+            }
+
+            return Ok(companyDtos);
         }
 
         [HttpGet("{companyId}")]
---
+++ Routine.Api/Models/CompanyDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routine.Api.Models
+{
+    public class CompanyDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        //public string Introduction { get; set; }
+
+    }
+}
