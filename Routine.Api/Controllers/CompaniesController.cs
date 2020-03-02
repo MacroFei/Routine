e755d1c221898a292dbddca8b07c7b102ff31493
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-03-02 16:51:33 +0800

    状态码、路由相关

---
+++ Routine.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Services;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompaniesController : Controller
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompaniesController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository ??
+                throw new ArgumentNullException(nameof(companyRepository));
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _companyRepository.GetCompaniesAsync();
+
+            return Ok(companies);
+        }
+
+        [HttpGet("{companyId}")]
+        public async Task<IActionResult> GetCompany(Guid companyId)
+        {
+           
+            var company = await _companyRepository.GetCompanyAsync(companyId);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(company);
+        }
+    }
+}
