c62e24b53cd1d5ab40bc4303013f250b84fde04c
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-03-06 09:50:42 +0800

    P26 输入验证和Data Annotations
    {
    	"name":null,
    	"Introduction":""
    }

--- Routine.Api/Models/CompanyAddDto.cs
+++ Routine.Api/Models/CompanyAddDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,14 @@ namespace Routine.Api.Models
 {
     public class CompanyAddDto
     {
+        [Display(Name = "公司名")]
+        [Required(ErrorMessage ="{0}这个字段是必填的")]
+        [MaxLength(100,ErrorMessage ="{0}的最大长度不可以超过{1}")]
         public string Name { get; set; }
+        [Display(Name="简介")]
+        //[MaxLength(500)]
+        //[MinLength(10)]
+        [StringLength(500,MinimumLength =10,ErrorMessage = "{0}的长度范围从{2}到{1}")]
         public string Introduction { get; set; }
 
         public ICollection<EmployeeAddDto> Employees { get; set; } = new List<EmployeeAddDto>();
